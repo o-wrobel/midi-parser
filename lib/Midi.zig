c578aad827b420910335ad324731fb25898d6ec3
o-wrobel <o.wrobel2025@protonmail.com>
2026-08-27 18:13:45 +0200

    Add more enum values and add Key enum

--- lib/Midi.zig
+++ lib/Midi.zig
@@ -29,6 +29,8 @@ pub fn deinit(self: Midi, allocator: std.mem.Allocator) void {
     allocator.free(self.tracks);
 }
 
+// Track
+
 pub const Track = struct {
     events: []TrackEvent,
 
@@ -48,11 +50,14 @@ pub const TrackEvent = struct {
     delta: u28, // Stored in file as a variable length value
     event: Event
 };
+
 pub const Event = union (enum) {
     pub const MidiMessage = struct {
         pub const Type = enum (u4) {
             note_off = 0b1000,
             note_on = 0b1001,
+            control_change = 0b1011,
+            program_change = 0b1100,
             _,
         };
         type: Type,
@@ -87,18 +92,22 @@ pub const Event = union (enum) {
     meta: Meta
 };
 
-
-pub const Format = enum (u16) {
-    single_track = 0,
-    simultaneous_tracks = 1,
-    separate_tracks = 2,
+pub const Key = enum (u8) {
+    c0 = 0, cs0, d0, ds0, e0, f0, fs0, g0, gs0, a0, as0, b0,
+    c1, cs1, d1, ds1, e1, f1, fs1, g1, gs1, a1, as1, b1,
+    c2, cs2, d2, ds2, e2, f2, fs2, g2, gs2, a2, as2, b2,
+    c3, cs3, d3, ds3, e3, f3, fs3, g3, gs3, a3, as3, b3,
+    c4, cs4, d4, ds4, e4, f4, fs4, g4, gs4, a4, as4, b4,
+    c5, cs5, d5, ds5, e5, f5, fs5, g5, gs5, a5, as5, b5,
+    c6, cs6, d6, ds6, e6, f6, fs6, g6, gs6, a6, as6, b6,
+    c7, cs7, d7, ds7, e7, f7, fs7, g7, gs7, a7, as7, b7,
+    c8, cs8, d8, ds8, e8, f8, fs8, g8, gs8, a8, as8, b8,
+    c9, cs9, d9, ds9, e9, f9, fs9, g9, gs9, a9, as9, b9,
+    c10, cs10, d10, ds10, e10, f10, fs10, g10,
 };
-pub const Division = packed struct (u16) {
-    const Type = enum (u1) { ticks, subdivisions};
 
-    format: Type,
-    value: u15,
-};
+// Header
+
 pub const Header = struct {
     length: u32,
     format: Format,
@@ -106,6 +115,19 @@ pub const Header = struct {
     ntkrs: u16,
     division: Division
 
+};
+
+pub const Format = enum (u16) {
+    single_track = 0,
+    simultaneous_tracks = 1,
+    separate_tracks = 2,
+};
+
+pub const Division = packed struct (u16) {
+    const Type = enum (u1) { ticks, subdivisions};
+
+    format: Type,
+    value: u15,
 };
 
 // Low level reading functions
@@ -123,7 +145,6 @@ fn readMidiFile(reader: *Io.Reader, allocator: Allocator) !Midi {
         .tracks = tracks
     };
 }
-
 
 fn readHeaderChunk(reader: *Io.Reader) !Header {
     const title = try reader.take(4);
